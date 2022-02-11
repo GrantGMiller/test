9dc3cb554d101f78731b632b6fe1e1ff69cf4126
Grant Miller <grant@grant-miller.com>
2022-02-11 16:08:31 -0500

    init commit

--- time_test.py
+++ time_test.py
@@ -1,6 +1,6 @@
 import sys
 import urllib.request
 
-resp = urllib.request.urlopen('https://grant-miller.com/notify?apiKey=2022-02-11&text={}'.format(
+resp = urllib.request.urlopen('https://gs.grant-miller.com/notify?apiKey=2022-02-11&text={}'.format(
     sys.platform
 ))
